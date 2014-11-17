ce9c23acecd37077a14544be4c36e728be90d84a
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2014-11-17 20:48:54 +0800

    change main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,14 @@ angular.module('tripApp')
       AniJS.run();
     }, 100);
 
+    $('.been-there').click(function(event) {
+      var parentDiv = $(event.target).parent('div');
+      var image = parentDiv.find('img');
+      $timeout(function() {
+        image.hide();
+        parentDiv.height(200);
+        parentDiv.css('background', 'red');
+      }, 500);
+    });
+
   });
